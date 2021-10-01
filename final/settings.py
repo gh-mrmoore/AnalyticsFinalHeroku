0dde1ed18c7c5dcbe41dfe81b32ef9195ab4db68
mrm <matt.rmoore@gmail.com>
2021-09-30 22:45:13 -0500

    Direct push to Heroku

--- final/settings.py
+++ final/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from .private import db_info, key_secret
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = key_secret
+SECRET_KEY = 'django-insecure-u@^)de#cjmg&_=(qyy^6t7dh1&9zf)h*7@o_v$@!nqrl(j14s='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -124,14 +123,12 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),   # changed from staticfiles to static to enable static files on 'final'
 )
 
-DISABLE_COLLECTSTATIC = 0
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+import django_heroku
 django_heroku.settings(locals())
 
 import dj_database_url
